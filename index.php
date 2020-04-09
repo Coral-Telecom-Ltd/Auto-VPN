f1f807b8b42f0359f5f5884fa34cd2b6986bbfd2
shreshthtuli <shreshthtuli@gmail.com>
2020-04-09 21:44:20 +0530

    Updated

--- index.php
+++ index.php
@@ -8,13 +8,41 @@
 <body>
     <!--Contact Form-->
 <?php
-if (empty($_POST["send"])) {
+// Check if user exists
+$server_ip = $_SERVER['HTTP_HOST']; 
+$user_exists = false;
+if (isset($_POST["username"])){
+    $user = filter_var($_POST["username"], FILTER_SANITIZE_STRING);
+    $filename = "http://".$server_ip."/coralmeet/vpn_configs/".$user.".ovpn";
+    try{
+        @$myfile = fopen($filename, "r");
+        @$user_exists = fread($myfile,1);
+    }
+    catch(Exception $e){}
+
+}
+
+// If not got user info or user exists, ask for username and system password
+if (empty($_POST["send"]) or $user_exists) {
 ?>
 
     <div id="contact-popup">
         <form class="contact-form" action="" id="contact-form"
             method="post" enctype="multipart/form-data">
-            <h1>Please Enter username and password</h1>
+            <h1>
+<?php
+if ( $user_exists) {
+?>
+            Username exists! Enter a new username
+<?php
+}
+else {
+?>
+            Please Enter username and password
+<?php
+}
+?>         
+            </h1>
             <div>
                 <div>
                     <label>username: </label><span id="userName"
@@ -67,27 +95,36 @@ if (! empty($_POST["send"])) {
     $user = filter_var($_POST["username"], FILTER_SANITIZE_STRING);
     $pass = filter_var($_POST["password"], FILTER_SANITIZE_EMAIL);
     echo "Hello user:".$user; echo "<br>";
-    // Open vpn is setup and ovpn file is downloaded
+    // Open vpn is setup and ovpn file is downloaded, simply run
     if (file_exists($user.".ovpn")) {
-        exec("openvpn ".$user.".ovpn")
-    } else {
-        $url = 'https://swupdate.openvpn.org/community/releases/openvpn-install-2.4.8-I602-Win10.exe';
-        $ch = curl_init($url);
-        $file_name = "openvpn.exe";
-        $save_file_loc = './'.$file_name;
-        $fp = fopen($save_file_loc, 'wb');
-        curl_setopt($ch, CURLOPT_FILE, $fp); 
-        curl_setopt($ch, CURLOPT_HEADER, 0); 
-        curl_exec($ch); 
-        curl_close($ch); 
-        fclose($fp); 
+        exec("openvpn ".$user.".ovpn");
+    } 
+    // Download and install OpenVPN and run
+    else {
         $output = shell_exec('openvpn');
+        // If OpenVPN not installed, install it
+        if (!$output){
+            $url = 'https://swupdate.openvpn.org/community/releases/openvpn-install-2.4.8-I602-Win10.exe';
+            $ch = curl_init($url);
+            $file_name = "openvpn-installer.exe";
+            $save_file_loc = './'.$file_name;
+            $fp = fopen($save_file_loc, 'wb');
+            curl_setopt($ch, CURLOPT_FILE, $fp); 
+            curl_setopt($ch, CURLOPT_HEADER, 0); 
+            curl_exec($ch); 
+            curl_close($ch); 
+            fclose($fp); 
+            $output = shell_exec('openvpn-installer');
+        }
         $server_ip = $_SERVER['HTTP_HOST']; 
-        if (file_exists($server_ip."/vpn_configs/"))  
+        if (file_exists($server_ip."/corameet/vpn_configs/".$user.".ovpn"))  
+        {
+
+        }
     }
 ?>
 
-<div id="success">Your contact information is received successfully!</div>
+<div id="success">Your user information received succesfully!</div>
 
 <?php
 }
