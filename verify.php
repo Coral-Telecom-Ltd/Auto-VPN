22f1c488f018b56fe70650be3f22b23dca5d9440
shreshthtuli <shreshthtuli@gmail.com>
2020-04-14 15:38:20 +0530

    Update verify.php

--- verify.php
+++ verify.php
@@ -79,7 +79,7 @@ else {
         echo "<a class='cls_copy_pg_action copyAction copy-action-btn' data-value='https://collaboration.coraltele.com/".$hash."'> <i class='far fa-copy'></i><center> Copy URL <img alt='copy' src='copy.png'
          width=15' height='15'> </center></a><br>";
          echo "<center><font size='1'>This solution has been developed using open-sources platforms including FreeSwitch, OpenVPN, GoogleCloudAI, Jitsi and OpenSwan</font></center>";
-         echo "<center><font size='1'><i>Developed by engineers at IIT Delhi and Coral Telecom Ltd.</i></font></center>";
+         echo "<center><font size='3'><i>Developed by QUBIT INC - an Indian startup of engineers from IIT Delhi and Coral Telecom</i></font></center>";
     }
 ?>
 
