7b65faab8041b77c8b4fb63d17d9949428c2232e
shreshthtuli <shreshthtuli@gmail.com>
2020-04-14 15:28:31 +0530

    Update verify.php

--- verify.php
+++ verify.php
@@ -78,7 +78,7 @@ else {
         echo $hash."' class='w3-btn w3-black'>https://collaboration.coraltele.com/".$hash."</a></center><br>";
         echo "<a class='cls_copy_pg_action copyAction copy-action-btn' data-value='https://collaboration.coraltele.com/".$hash."'> <i class='far fa-copy'></i><center> Copy URL <img alt='copy' src='copy.png'
          width=15' height='15'> </center></a>";
-         echo "<center><font size='5'>This solution has been developed using open-sources platforms including FreeSwitch, OpenVPN, GoogleCloudAI, Jitsi, OpenSwan</font></center>";
+         echo "<center><font size='2'>This solution has been developed using open-sources platforms including FreeSwitch, OpenVPN, GoogleCloudAI, Jitsi and OpenSwan</font></center>";
     }
 ?>
 
